35cdc63e038371460c90b7ab064cd5502c22162f
alphaClass6143 <alphaClass@web.de>
2023-01-10 02:01:19 +0100

    Added category model and data for it

--- pcwareurs/category/models.py
+++ pcwareurs/category/models.py
@@ -1,3 +1,30 @@
+'''
+Models for the category app
+'''
+from datetime import datetime
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    '''
+    Category model
+    '''
+    name = models.CharField(
+        max_length=255
+    )
+
+    handle = models.CharField(
+        max_length=255
+    )
+
+    description = models.TextField()
+
+    created_at = models.DateTimeField(
+        default=datetime.now
+    )
+
+    modified_at = models.DateTimeField(
+        null=True,
+        blank=True
+    )
