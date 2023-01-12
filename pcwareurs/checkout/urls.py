a5d467dda0e573bb838444b7e16ded867b25e15e
alphaClass6143 <alphaClass@web.de>
2023-01-12 06:09:06 +0100

    Added basic checkout layout

--- pcwareurs/checkout/urls.py
+++ pcwareurs/checkout/urls.py
@@ -1,7 +1,14 @@
 '''
 Urlpatterns
 '''
+from django.urls import path
+
+from checkout import views
+
 
 urlpatterns = [
-
+    path('checkout/next_step', views.next_step, name='next_step'),
+    path('checkout/previous_step', views.previous_step, name='previous_step'),
+    path('checkout/load_step', views.load_step, name='load_step'),
+    path('checkout/cancel_step', views.cancel_step, name='cancel_step')
 ]
--- pcwareurs/checkout/views.py
+++ pcwareurs/checkout/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def load_step(request):
+    '''
+    Load step
+    '''
+    step = request.session.get('step')
+    print(step)
+
+    if step == 1:
+        return render(request,
+                      'checkout/cart_step.html')
+
+    elif step == 2:
+        return render(request,
+                      'checkout/address_step.html')
+
+    elif step == 3:
+        return render(request,
+                      'checkout/payment_step.html')
+
+    elif step == 4:
+        return render(request,
+                      'checkout/confirm_step.html')
+    
+    else:
+        # Step does not exist
+        request.session['step'] = 1
+        return render(request,
+                      'checkout/cart_step.html')
+
+
+def previous_step(request):
+    '''
+    Previous step
+    '''
+    step = request.session.get('step')
+    if not step <= 1 and not step > 4:
+        request.session['step'] = step - 1
+        print("redirecting")
+        return redirect('load_step')
+
+    return render(request,
+                  'home/index.html')
+
+
+def next_step(request):
+    '''
+    Next step
+    '''
+    step = request.session.get('step')
+    if not step < 1 and not step >= 4:
+        request.session['step'] = step + 1
+        return redirect('load_step')
+        
+    return render(request,
+                  'home/index.html')
+
+
+def cancel_step(request):
+    '''
+    Cancel step
+    '''
+    del request.session['step']
+        
+    return render(request,
+                  'home/index.html')
+
