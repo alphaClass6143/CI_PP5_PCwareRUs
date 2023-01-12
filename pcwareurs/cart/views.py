963c4776dae7005ff884cad53fcd72a586c1f412
alphaClass6143 <alphaClass@web.de>
2023-01-12 02:38:28 +0100

    Added add to cart functionality

--- pcwareurs/cart/views.py
+++ pcwareurs/cart/views.py
@@ -1,9 +1,13 @@
 '''
 Cart views
 '''
+import json
+
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.forms.models import model_to_dict
 from product.models import Product
+from django.template.loader import render_to_string
 
 
 
@@ -13,18 +17,27 @@ def cart_add(request):
     Adds product to cart
     '''
     if request.method == "POST":
-        product_id = request.POST['product_id']
-        quantity = request.POST['quantity']
-
+        print(request.body)
+        data = json.loads(request.body)
+        product_id = data['product_id']
+        quantity = data['quantity']
+        
         if Product.objects.filter(id=product_id).exists():
+            product = Product.objects.get(id=product_id)
             cart = request.session.get('cart', {})
-            if product_id in cart:
-                cart[product_id] += int(quantity)
+
+            if str(product.id) in cart.keys():
+                cart[str(product.id)]["quantity"] += int(quantity)
             else:
-                cart[product_id] = int(quantity)
+                cart[str(product.id)] = {
+                    "product_id": product_id,
+                    "product_name": product.product_name,
+                    "product_image": "nope",
+                    'quantity': int(quantity)
+                }
 
             request.session['cart'] = cart
-            return JsonResponse({'success': True})
+            return render_cart(request)
         return JsonResponse({'error': 'Product does not exist'})
     return JsonResponse({'error': 'Invalid request method'})
 
@@ -58,3 +71,13 @@ def cart_update(request):
         request.session['cart'] = cart
         return JsonResponse({'success': True})
     return JsonResponse({'error': 'Product not in cart'})
+
+
+def render_cart(request):
+    """
+    Renders the cart HTML and returns a JSON response
+    """
+    cart = request.session.get('cart', {})
+    print(cart)
+    cart_html = render_to_string('cart/cart.html', {'cart': cart})
+    return JsonResponse({'success': True, 'cart_html': cart_html})
--- pcwareurs/static/script/cart.js
+++ pcwareurs/static/script/cart.js
@@ -3,3 +3,29 @@ document.querySelector('#cart-btn').addEventListener('click', function() {
     document.querySelector('#cart').classList.toggle('cart-toggle');
     console.log("Used")
 });
+
+
+function addToCart(productId, quantity) {
+    let csrf_token = document.getElementById("csrf_token").value;
+
+    return fetch('/cart/add/', {
+        method: 'POST',
+        headers: { 
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrf_token 
+        },
+        body: JSON.stringify({ 'product_id': productId, 'quantity': quantity })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // handle the response from the server
+        if (data.success) {
+            console.log("success")
+            document.querySelector('#cart').classList.toggle('cart-toggle');
+            document.querySelector('#cart').innerHTML = data.cart_html
+        } else {
+            // show an error message
+            console.log(data)
+        }
+    });
+}
