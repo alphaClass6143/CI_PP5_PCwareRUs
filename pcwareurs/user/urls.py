4cee1eaf3f16cb51c6bd8e67228eef52a2182bb6
alphaClass6143 <alphaClass@web.de>
2023-01-15 17:24:00 +0100

    Added edit address view

--- pcwareurs/user/urls.py
+++ pcwareurs/user/urls.py
@@ -4,8 +4,15 @@ Urlpatterns
 
 from django.urls import path
 from . import views
+# from .views import (
+#     AddressUpdateView,
+# )
+
 
 urlpatterns = [
     path('user/user_overview', views.user_overview, name='user_overview'),
     path('user/add_address', views.add_address, name='add_address'),
+    path('user/edit_address/<int:address_id>', views.edit_address, name='edit_address'),
+    # path('user/edit_address/<int:pk>', AddressUpdateView.as_view(), name='edit_address'),
+    path('user/delete_address/<int:address_id>', views.delete_address, name='delete_address'),
 ]
--- pcwareurs/user/views.py
+++ pcwareurs/user/views.py
@@ -3,7 +3,14 @@ User views
 '''
 from datetime import datetime
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+    ListView
+)
 
 from user.forms import AddressForm
 
@@ -46,3 +53,54 @@ def add_address(request):
         return render(request, 'user/add_address.html')
     # TODO: Add restricted access message
     return redirect('home')
+
+
+def edit_address(request, address_id):
+    '''
+    Edit Address
+    '''
+    if request.user.is_authenticated:
+        address = get_object_or_404(Address, pk=address_id)
+
+        if address.user == request.user:
+
+            if request.method == 'POST':
+            
+                form = AddressForm(request.POST)
+                
+                if form.is_valid():
+                    if address.is_used:
+                        Address.objects.create(
+                            user=request.user,
+                            street=form.cleaned_data['street'],
+                            city=form.cleaned_data['city'],
+                            state=form.cleaned_data['state'],
+                            zip=form.cleaned_data['zip'],
+                            country=form.cleaned_data['country']
+                        )
+                    else:
+                        address.street = form.cleaned_data['street']
+                        address.city = form.cleaned_data['city']
+                        address.state = form.cleaned_data['state']
+                        address.zip = form.cleaned_data['zip']
+                        address.country = form.cleaned_data['country']
+                        address.save()
+
+                    return redirect('user_overview')
+                print(form.errors)
+                return render(request, 'user/edit_address.html', {'error_message': 'Invalid input'})
+            return render(request, 'user/edit_address.html', {'address': address})
+        # TODO: Add restricted access message
+        return redirect('user_overview')
+    # TODO: Add restricted access message
+    return redirect('home')
+
+
+def delete_address(request, address_id):
+    if request.user.is_authenticated:
+        address = get_object_or_404(Address, pk=address_id)
+
+        if address.user == request.user:
+
+            if request.method == 'DELETE':
+                pass
