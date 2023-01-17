ca5b30eb592ce392c4af97892e85076f87a89de2
alphaClass6143 <alphaClass@web.de>
2023-01-17 03:27:33 +0100

    Added edit review view

--- pcwareurs/product/views.py
+++ pcwareurs/product/views.py
@@ -48,3 +48,32 @@ def add_review(request, product_id):
                   'home/index.html',
                   {'error_message': 'Invalid request'})
 
+
+def edit_review(request, review_id):
+    '''
+    Edit review route
+    '''
+    review = Review.objects.get(id=review_id)
+
+    if request.user == review.user:
+        if request.method == 'POST':
+            form = ReviewEditForm(request.POST)
+            if form.is_valid():
+
+                review.content = form.cleaned_data['content']
+                review.modified_at = datetime.now()
+                review.save()
+                return redirect('product_detail', review_id=review.product.id)
+
+            return render(request,
+                          'product/edit_review.html',
+                          {'error_message': 'Invalid input'})
+
+        return render(request,
+                      'product/edit_review.html',
+                      {'review': review})
+
+    return render(request,
+                  'product/product_detail.html',
+                  {'product_detail': review.product,
+                   'error_message': 'Really? No you cannot edit this review, this is not your review!'})
